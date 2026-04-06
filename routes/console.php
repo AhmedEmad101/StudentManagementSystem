6e4a6902970518399b815743243ed60bcd1bc6c6
AhmedEmad101 <emada2826@gmail.com>
2026-04-06 15:46:03 +0200

    adding continous prompting with the agent

--- routes/console.php
+++ routes/console.php
@@ -4,12 +4,14 @@ use App\Ai\Agents\PersonalAssistant;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Models\User;
+use function Laravel\Prompts\text;
 Artisan::command('try_ai', function () {
     $user = User::query()->first();
     while(true){
-  $response = PersonalAssistant::make()
-  ->continue('first_conversation', as:$user)
-  ->prompt('what is my name');
-  $this->info((string) $response);
+    $prompt = text('Prompt:');
+    $response = PersonalAssistant::make()
+    ->continue('first_conversation', as:$user)
+    ->prompt('what is my name');
+    $this->info((string) $response);
     }
 });
